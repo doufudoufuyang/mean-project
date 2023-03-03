886f7611fd01c0e1b6f540f619a233c711c3c45e
Yujia-Wan <yujiawan@andrew.cmu.edu>
2023-03-03 13:14:41 -0500

    Refactor House Model, add Report Model

---
+++ server/models/Report.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const CommentSchema = new Schema({
+    description: { type: String, required: true },
+    createdBy: { type: refType, ref: 'User', required: true },
+    timestamp: { type: Date, required: true },
+})
+
+const ReportSchema = new Schema({
+    title: { type: String, required: true },
+    date: { type: Date, required: true },
+    status: { type: String, enum: ['Open', 'In Progress', 'Closed'], required: true },
+    description: { type: String, required: true },
+    createdBy: { type: refType, ref: 'User', required: true },
+    comments: [{ type: CommentSchema }],
+})
+
+const Report = mongoose.model('Report', ReportSchema, 'Report');
+
+module.exports = Report;
