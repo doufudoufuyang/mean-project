993de5a5e97aa02e60b54a9e0d29e84632655f4f
Yujia-Wan <yujiawan@andrew.cmu.edu>
2023-03-06 15:40:01 -0500

    Refactor client code

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,21 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
-import { OnboardComponent } from './components/onboard/onboard.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { EmployeePersonalInfoComponent } from './components/employee-personal-info/employee-personal-info.component';
-import { EmployeeVisaStatusComponent } from './components/employee-visa-status/employee-visa-status.component';
-import { HrHomeComponent } from './components/hr-home/hr-home.component';
-import { HrEmployeeProfilesComponent } from './components/hr-employee-profiles/hr-employee-profiles.component';
-import { HrVisaManagementComponent } from './components/hr-visa-management/hr-visa-management.component';
-import { HrHiringManagementComponent } from './components/hr-hiring-management/hr-hiring-management.component';
-import { HrHousingManagementComponent } from './components/hr-housing-management/hr-housing-management.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { OnboardComponent } from './pages/onboard/onboard.component';
+import { HeaderComponent } from './pages/header/header.component';
+import { FooterComponent } from './pages/footer/footer.component';
+import { EmployeePersonalInfoComponent } from './pages/employee-personal-info/employee-personal-info.component';
+import { EmployeeVisaStatusComponent } from './pages/employee-visa-status/employee-visa-status.component';
+import { HrHomeComponent } from './pages/hr-home/hr-home.component';
+import { HrEmployeeProfilesComponent } from './pages/hr-employee-profiles/hr-employee-profiles.component';
+import { HrVisaManagementComponent } from './pages/hr-visa-management/hr-visa-management.component';
+import { HrHiringManagementComponent } from './pages/hr-hiring-management/hr-hiring-management.component';
+import { HrHousingManagementComponent } from './pages/hr-housing-management/hr-housing-management.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
@@ -32,6 +33,7 @@ import { HrHousingManagementComponent } from './components/hr-housing-management
     HrVisaManagementComponent,
     HrHiringManagementComponent,
     HrHousingManagementComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
