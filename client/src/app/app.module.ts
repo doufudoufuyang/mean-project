1f2e703bdc53f70266c11eebe9d9b748673a9edc
Yuhao Yang <kaokaoyang030@gmail.com>
2023-03-06 15:59:54 -0800

    complete employee register

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -16,6 +17,12 @@ import { HrEmployeeProfilesComponent } from './components/hr-employee-profiles/h
 import { HrVisaManagementComponent } from './components/hr-visa-management/hr-visa-management.component';
 import { HrHiringManagementComponent } from './components/hr-hiring-management/hr-hiring-management.component';
 import { HrHousingManagementComponent } from './components/hr-housing-management/hr-housing-management.component';
+
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+
 
 @NgModule({
   declarations: [
@@ -34,9 +41,15 @@ import { HrHousingManagementComponent } from './components/hr-housing-management
     HrHousingManagementComponent,
   ],
   imports: [
+    FormsModule,
+    ReactiveFormsModule,
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client/src/app/components/register/register.component.ts
+++ client/src/app/components/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +9,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  registerToken : string | undefined
+  registerForm : FormGroup = this.formBuilder.group({
+    username: ['', Validators.required],
+    email: ['', Validators.required],
+    password: ['', Validators.required]
+  })
 
+  constructor(
+    private route : ActivatedRoute,
+    private router : Router,
+    private formBuilder : FormBuilder,
+    private http : HttpClient
+    ) {}
+
+  ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      console.log('1111: ', params['token'])
+      this.registerToken = params['token']
+      console.log('register token: ', this.registerToken)
+    })
+  }
+
+  register(){
+    console.log(this.registerForm.getRawValue())
+
+    const requestBody = this.registerForm.getRawValue()
+    let token = '' 
+    if (this.registerToken){
+      token = `jwt ${this.registerToken}`
+    }
+    this.http.post('http://localhost:3000/user/register', requestBody, {
+      headers : {'authorization' : token}
+    }).subscribe((data) => {
+      console.log(data)
+      this.router.navigate(['login'])
+    }, (error) => {
+      console.log('error', error)
+    })
+  }
 }
